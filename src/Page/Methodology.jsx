362aadc5b70f637b4c047e1069636793aeb4a223
Adarsh tripathi <adarshtripathi2070@gmail.com>
2024-07-26 14:06:34 +0530

    commit

--- src/Page/Methodology.jsx
+++ src/Page/Methodology.jsx
@@ -13,9 +13,9 @@ const Methodology = () => {
             <div className="img">
               <img src="https://media.istockphoto.com/id/1480277406/photo/graduation-group-and-back-view-of-students-celebrate-education-success-behind-of-excited.webp?b=1&s=170667a&w=0&k=20&c=VjQgIGeJIlMh-KTrfLxf14u_0uq7CjpVFlCG4X0snkY=" alt=""  className='w-[35rem] md:w-[50rem] h-[25rem]' />
             </div>
-            <div className="bg-white rounded-lg p-6 flex flex-col justify-end items-center">
+            <div className="bg-white rounded-lg p-6 flex flex-col justify-center items-center">
               <h1 className='text-3xl w-[50rem] font-semibold my-5 text-center'>Student-Centered Learning</h1>
-              <p className='text-2xl w-[35rem] md:w-[50rem] h-[25rem]'>We focus on creating a learning environment where students are actively engaged in their education. Through inquiry-based learning, project-based learning, and collaborative activities, we empower students to become independent thinkers.</p>
+              <p className='text-2xl w-[35rem] md:w-[50rem] h-[25rem] text-center'>We focus on creating a learning environment where students are actively engaged in their education. Through inquiry-based learning, project-based learning, and collaborative activities, we empower students to become independent thinkers.</p>
             </div>     
           </div>
  
@@ -25,9 +25,9 @@ const Methodology = () => {
             <div className="img">
               <img src="https://media.istockphoto.com/id/1480277406/photo/graduation-group-and-back-view-of-students-celebrate-education-success-behind-of-excited.webp?b=1&s=170667a&w=0&k=20&c=VjQgIGeJIlMh-KTrfLxf14u_0uq7CjpVFlCG4X0snkY=" alt=""  className='w-[35rem] md:w-[50rem] h-[25rem]' />
             </div>
-            <div className="bg-white rounded-lg p-6 flex flex-col justify-end items-center">
+            <div className="bg-white rounded-lg p-6 flex flex-col justify-center items-center ">
               <h1 className='text-3xl w-[50rem] font-semibold my-2 text-center'>Technology Integration</h1>
-              <p className='text-2xl w-[35rem] md:w-[50rem] '>We leverage technology to enhance the learning experience. Our classrooms are equipped with interactive whiteboards, and students have access to online resources and digital tools to support their learning.</p>
+              <p className='text-2xl w-[35rem] md:w-[50rem] text-center'>We leverage technology to enhance the learning experience. Our classrooms are equipped with interactive whiteboards, and students have access to online resources and digital tools to support their learning.</p>
             </div>
           </div>
 
